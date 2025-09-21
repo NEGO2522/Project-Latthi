75d4255a8777d809e0998aca99ee994744622068
cottonfab0001 <cottonfab0001@gmail.com>
2025-09-21 11:14:21 +0000

    Updated Everything

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -12,18 +12,15 @@ import {
 } from "firebase/auth";
 import { getDatabase, ref, set, push, onValue, remove, get } from "firebase/database";
 
-// Import environment variables
-import env from '../env';
-
 // Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: env.FIREBASE_API_KEY,
-  authDomain: env.FIREBASE_AUTH_DOMAIN,
-  projectId: env.FIREBASE_PROJECT_ID,
-  storageBucket: env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: env.FIREBASE_APP_ID,
-  measurementId: env.FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
   // Realtime Database URL - Using default region
   databaseURL: "https://e-commerce-9ddae-default-rtdb.firebaseio.com"
 };
