440b796018a1fd8a1e46759fd2b11bd20cc93fda
cottonfab0001 <cottonfab0001@gmail.com>
2025-09-24 04:54:22 +0000

    Updated all th things

--- vite.config.js
+++ vite.config.js
@@ -2,34 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
-      },
-    }),
+  plugins: [react(),
     tailwindcss()
   ],
-  server: {
-    port: 5173,
-    strictPort: true,
-    open: true
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom', 'react-router-dom'],
-          vendor: ['@emotion/react', '@emotion/styled', '@mui/material'],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
-  },
-  optimizeDeps: {
-    include: ['@emotion/react', '@emotion/styled', '@mui/material'],
-  },
+  base: './',
 })
