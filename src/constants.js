fd4046826d1c99bf6433b4ce5b9fd13ae3f0627e
cottonfab0001 <cottonfab0001@gmail.com>
2025-09-25 05:05:52 +0000

    Updated Pyemtn

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,8 @@
 
-export const CATEGORIES = ["T-Shirts", "Shirts", "Kurtis"];
+export const CATEGORIES = [
+  { name: 'One Piece', value: 'one-piece' },
+  { name: 'Two Piece', value: 'two-piece' },
+  { name: 'Three Piece', value: 'three-piece' },
+  { name: 'Short Kurti', value: 'short-kurti' },
+];
 export const AVAILABLE_SIZES = ["S", "M", "L", "XL", "XXL"];
